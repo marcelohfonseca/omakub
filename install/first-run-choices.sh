615cf6c12719244ce2c595f38f46dc571a5cf3d9
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-11-02 17:26:10 -0300

    Update first-run-choices.sh

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -6,6 +6,6 @@ AVAILABLE_LANGUAGES=("Python" "R" "Julia" "Scala" "Java")
 SELECTED_LANGUAGES="Python"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
-AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL", "SQLServer")
+AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL" "SQLServer")
 SELECTED_DBS="SQLServer"
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
