25173497d6c90cd72c11b55ccf81770cad24626b
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-11-02 17:37:56 -0300

    Update select-dev-storage.sh

--- install/terminal/select-dev-storage.sh
+++ install/terminal/select-dev-storage.sh
@@ -2,13 +2,19 @@
 if [[ -v OMAKUB_FIRST_RUN_DBS ]]; then
 	dbs=$OMAKUB_FIRST_RUN_DBS
 else
-	AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL" "SQLServer")
+	AVAILABLE_DBS=("SQLServer" "MySQL" "Redis" "PostgreSQL")
 	dbs=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --height 5 --header "Select databases (runs in Docker)")
 fi
 
 if [[ -n "$dbs" ]]; then
 	for db in $dbs; do
 		case $db in
+		SQLServer)
+			docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" \
+			-p 1433:1433 --name mssql --hostname mssql \
+			-d \
+			mcr.microsoft.com/mssql/server:2022-latest
+			;;
 		MySQL)
 			sudo docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
 			;;
@@ -17,12 +23,6 @@ if [[ -n "$dbs" ]]; then
 			;;
 		PostgreSQL)
 			sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
-			;;
-		SQLServer)
-			docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" \
-			-p 1433:1433 --name mssql --hostname mssql \
-			-d \
-			mcr.microsoft.com/mssql/server:2022-latest
 			;;
 		esac
 	done
