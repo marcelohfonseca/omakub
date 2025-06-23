8d83c15bf3bc1f05cd33ca1bba20d6681216384d
Marcelo Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2025-06-23 20:58:38 -0300

    Update docker.sh

--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -10,6 +10,7 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 
 # Give this user privileged Docker access
 sudo usermod -aG docker ${USER}
+newgrp docker
 
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
