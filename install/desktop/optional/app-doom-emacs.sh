ef4edd8d800ca709c9d5dda45d29b7f594cc4576
Marcelo Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2025-06-23 11:13:47 -0300

    Update app-doom-emacs.sh

--- install/desktop/optional/app-doom-emacs.sh
+++ install/desktop/optional/app-doom-emacs.sh
@@ -1,3 +1,3 @@
-sudo apt install -y emacs
+sudo DEBIAN_FRONTEND=noninteractive apt install -y emacs # não solicitar configuração do postfix
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
