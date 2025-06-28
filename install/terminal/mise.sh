b8aafe6be77e5679361740c4402f1625cca33ac3
Marcelo Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2025-06-28 11:14:02 -0300

    Update mise.sh

--- install/terminal/mise.sh
+++ install/terminal/mise.sh
@@ -5,3 +5,6 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt update
 sudo apt install -y mise
+
+# Add extra plugins (https://github.com/mise-plugins/registry)
+mise plugins add r
